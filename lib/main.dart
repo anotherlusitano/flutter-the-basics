762810044328b1111568cdc19f11c64ad0abebd0
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-04-24 15:30:37 +0100

    Finished lesson 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'models/location.dart';
 import 'location_detail.dart';
+import 'mocks/mock_location.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LocationDetail(),
-    ),
-  );
+  final Location mockLocation = MockLocation.FetchAny();
+
+  return runApp(MaterialApp(home: LocationDetail(mockLocation)));
 }
